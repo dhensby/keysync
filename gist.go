fe77c9389896faf5e0e05896a0bc16210280b91b
Dan Hensby <git@hens.by>
2018-09-20 00:28:43 +0100

    Small progress towards modifying the authorized_keys file

--- gist.go
+++ gist.go
@@ -8,13 +8,14 @@ import (
 
 func getGist(gistid string) string {
 	client := github.NewClient(nil)
+	// @todo look into caching mechanism
 	gist, _, err := client.Gists.Get(context.Background(), gistid)
 	if err != nil {
 		log.Fatalf("Failed to fetch gist: %s\n", err)
 	}
 	var keys = "";
 	for _, v := range gist.Files {
-		keys += v.String()
+		keys += *v.Content
 	}
 	return keys
 }
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	flag "github.com/ogier/pflag"
+	"io/ioutil"
 	"os"
+	"strings"
+	"os/user"
 )
 
 var (
 	gist string
-	user string
+	username string
 )
 
 func main() {
@@ -20,12 +24,48 @@ func main() {
 		os.Exit(1)
 	}
 
+	userDir := getUserDir()
+
+	// try to find authorized_keys file
+	// create it if it doesn't exist with correct perms / owner
+	// look in file for start marker
+	// remove contents from start marker to end marker
+	// insert new keys between markers
+	// write authorized_keys file with correct perms / owner
+	ioutil.ReadDir(userDir)
+
 	fmt.Printf("Loading public keys from gist: %s\n", gist)
 	result := getGist(gist)
-	fmt.Println(result)
+
+	scanner := bufio.NewScanner(strings.NewReader(result))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+		fmt.Println(line)
+	}
 }
 
 func init() {
 	flag.StringVarP(&gist, "gist", "g", "", "The gist to use as the source of your public keys")
-	flag.StringVarP(&user, "user", "u", "", "The user to manage the authorised keys of")
+	flag.StringVarP(&username, "user", "u", "", "The user to manage the authorised keys of")
+}
+
+func getUserDir() string {
+	var osUser *user.User
+	var err error
+	if username == "" {
+		osUser, err = user.Current()
+		fmt.Printf("No user supplied, assuming %s\n", osUser.Username)
+	} else {
+		osUser, err = user.Lookup(username)
+	}
+
+	if err != nil {
+		fmt.Println("Failed to get user")
+	}
+
+	return osUser.HomeDir
 }
